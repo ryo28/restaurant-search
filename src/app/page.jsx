f4acb28448913e5958829753687bde70a5570b86
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-09 15:13:30 +0900

    ♻️ 不要なものを作成しました

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
+import { Search } from "@/components/Search";
+
 export default function Home() {
   return (
     <>
-      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-blue-500 text-red-500">
-        テスト
-      </div>
+      <Search />
     </>
   );
 }
