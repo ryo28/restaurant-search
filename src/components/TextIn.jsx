35db61aa246c122b6dbed64d9c6e975a79cbb384
ryo28 <wakisan1@ezweb.ne.jp>
2023-09-11 16:54:25 +0900

    ✨　検索ボックスとボタンを作成しました

--- src/components/TextIn.jsx
+++ src/components/TextIn.jsx
@@ -1,8 +1,34 @@
-export function TextIn() {
+"use client";
 
+import { useState } from "react";
+
+export function TextIn() {
+  const [inputValue, setInputValue] = useState("");
+  const [displayText, setDisplayText] = useState("");
+
+  const handleInputChange = (event) => {
+    //入力されたテキストをstateに入れる
+    setInputValue(event.target.value);
+  };
+  //ボタンを押したらstateを変数に入れて返す
+  const handelSearchClick = () => {
+    setDisplayText(inputValue);
+    console.log(inputValue);
+    return displayText;
+  };
   return (
     <>
-      <div></div>
+      <div className="mt-2 flex justify-center space-x-1">
+        <input
+          className="border border-black"
+          type="text"
+          onChange={handleInputChange}
+          placeholder="お店の名前を入力"
+        />
+        <button className="border border-black" onClick={handelSearchClick}>
+          検索
+        </button>
+      </div>
     </>
   );
 }
